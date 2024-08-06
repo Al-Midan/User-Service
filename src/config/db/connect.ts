91858a8fc9881f486daf8ed8041fc1af5b4a0362
Muhammed Easa <easanedumangad@gmail.com>
2024-08-06 18:13:16 +0530

    Few Changes

--- src/config/db/connect.ts
+++ src/config/db/connect.ts
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("MONGO_URL from env:", process.env.MONGO_URL);
+console.log("MONGO_URL from env:",process.env.MONGO_URL);
 
 if(!process.env.MONGO_URL){
     console.log("MONGO_URL is not set, using fallback URL");
 }
 
-const mongoUrl = process.env.MONGO_URL || "mongodb+srv://easanedumangad:ByKn7EYAqmsgqTl0@cluster0.eyvqmzv.mongodb.net/Al-Midan?retryWrites=true&w=majority";
+const mongoUrl=process.env.MONGO_URL||"mongodb+srv://easanedumangad:ByKn7EYAqmsgqTl0@cluster0.eyvqmzv.mongodb.net/Al-Midan?retryWrites=true&w=majority";
 
 console.log("Final MONGO_URL:", mongoUrl);
 
